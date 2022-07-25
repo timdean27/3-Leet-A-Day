8921733575170321b34660e7dde9f1bdb859618d
timdean27 <timdean27@yahoo.com>
2022-07-25 11:55:16 -0400

    completed 1748. Sum of Unique Elements , Runtime: 124 ms, faster than 7.84% of JavaScript online submissions for Sum of Unique Elements.

--- 1748-Sum-of_unigue-Elements.js
+++ 1748-Sum-of_unigue-Elements.js
@@ -38,6 +38,6 @@ var sumOfUnique = function(nums) {
 };
 
 
-console.log(sumOfUnique ([1,2,3,2]))
-console.log(sumOfUnique ([1,1,1,1,1]))
-console.log(sumOfUnique ([1,2,3,4,5]))
+console.log(sumOfUnique ([1,2,3,2]));
+console.log(sumOfUnique ([1,1,1,1,1]));
+console.log(sumOfUnique ([1,2,3,4,5]));

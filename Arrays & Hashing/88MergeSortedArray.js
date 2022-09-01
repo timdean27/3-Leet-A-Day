3b04e26e53a4d77d182c0e383798eb3d05f1ecbf
timdean27 <timdean27@yahoo.com>
2022-09-01 14:57:03 -0400

    88 Merge Sorted Array

--- Arrays & Hashing/88MergeSortedArray.js
+++ Arrays & Hashing/88MergeSortedArray.js
@@ -42,8 +42,8 @@ var merge = function(nums1, m, nums2, n) {
     console.log(nums1)
 }
 
-// console.log(merge([1,2,3,0,0,0], 3,[2,5,6],3))
-// console.log(merge([1],1,[],0))
-// console.log(merge([0],0,[1],0))
-// console.log(merge([-1,0,0,3,3,3,0,0,0], 6,[1,2,2],3))
+console.log(merge([1,2,3,0,0,0], 3,[2,5,6],3))
+console.log(merge([1],1,[],0))
+console.log(merge([0],0,[1],0))
+console.log(merge([-1,0,0,3,3,3,0,0,0], 6,[1,2,2],3))
 console.log(merge([0,0,3,0,0,0,0,0,0],3, [-1,1,1,1,2,3],6))

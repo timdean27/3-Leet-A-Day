2ebbdba238a26937b7d3e7d45974f6c64d9b9e69
timdean27 <timdean27@yahoo.com>
2022-10-03 14:32:48 -0400

    working on 198 Hourse Robber Dynamic Prog DP

--- 1-D Dynamic Programming/198HouseRobber.js
+++ 1-D Dynamic Programming/198HouseRobber.js
@@ -22,36 +22,64 @@
 // Total amount you can rob = 2 + 9 + 1 = 12.
 
 
-
 let rob =(nums) =>{
     if(!nums.length) return 0
     if(nums.length === 1) return nums[0]
-    if(nums.length === 2) return Math.max(nums[0], nums[1])
-
-    for(let i = 2; i < nums.length; i++) {
-        // we will start at i = 2 and nums = [2,1,2,1,5,2,1]
-        // we will check the larger of  nums[i-2]+nums[i], (nums[i-3] || 0) + nums[i])
-        // so current number + 2 spaces before or current number + 3 spaces before 
-        // so for first case when i = 2 nums[0]+nums[2] = 2+2 , nums[2-3] || 0 + nums[2] = 0+2
-        // so nums[2] is set to 4 and nums = [2,1,4,1,5,2,1]
-
-        //when i = 3  nums[1]+nums[3] = 1+1 , nums([3-3] || 0) + nums[3] = 2+1
-        // so nums[3] is set to 3 and nums = [2,1,4,3,5,2,1]
-
-        //when i = 4  nums[2]+nums[4] = 4+5 , nums([4-3] || 0) + nums[4] = 1+5
-        // so nums[4] is set to 9 and nums = [2,1,4,3,9,2,1]
-
-        //when i = 5  nums[3]+nums[5] = 3+2 , nums([5-3] || 0) + nums[5] = 4+2
-        // so nums[5] is set to 6 and nums = [2,1,4,3,9,6,1]
-
-        //when i = 6  nums[4]+nums[6] = 9+1 , nums([6-3] || 0) + nums[6] = 3+1
-        // so nums[6] is set to 10 and nums = [2,1,4,3,9,6,10]
-
-        nums[i] = Math.max(nums[i-2]+nums[i], (nums[i-3] || 0) + nums[i])
-        console.log(nums)
+    let DP = Array(nums+1).fill(0)
+    DP[0] = nums[0];
+    DP[1] = Math.max(nums[0], nums[1]);
+ for (let i = 2; i < nums.length; i++){
+        // in this case DP starts at DP[0,1] = [2,2]
+        // when i = 2 DP[2] is either nums[2]+DP[2-2]  = 2+2, DP[2-1] = 2
+        //DP = [2,2,4]
+        // when i = 3 DP[3] is either nums[3]+DP[3-2]  = 1+2, DP[3-1] = 4
+        //DP = [2,2,4,4]
+        // when i = 4 DP[4] is either nums[4]+DP[4-2]  = 5+4, DP[4-1] = 4
+        //DP = [2,2,4,4,9]
+         // when i = 5 DP[5] is either nums[5]+DP[5-2]  = 2+4, DP[5-1] = 9
+        //DP = [2,2,4,4,9,9]
+         // when i = 6 DP[6] is either nums[6]+DP[6-2]  = 9+1, DP[6-1] = 9
+        //DP = [2,2,4,4,9,9,10]
+        DP[i] = Math.max(nums[i]+DP[i-2], DP[i-1])
+        console.log(DP)
     }
-    return Math.max(nums[nums.length-1], nums[nums.length-2])
-
+    return DP[DP.length-1]
 }
 
 console.log(rob([2,1,2,1,5,2,1]))
+
+
+
+
+// let rob =(nums) =>{
+//     if(!nums.length) return 0
+//     if(nums.length === 1) return nums[0]
+//     if(nums.length === 2) return Math.max(nums[0], nums[1])
+
+//     for(let i = 2; i < nums.length; i++) {
+//         // we will start at i = 2 and nums = [2,1,2,1,5,2,1]
+//         // we will check the larger of  nums[i-2]+nums[i], (nums[i-3] || 0) + nums[i])
+//         // so current number + 2 spaces before or current number + 3 spaces before 
+//         // so for first case when i = 2 nums[0]+nums[2] = 2+2 , nums[2-3] || 0 + nums[2] = 0+2
+//         // so nums[2] is set to 4 and nums = [2,1,4,1,5,2,1]
+
+//         //when i = 3  nums[1]+nums[3] = 1+1 , nums([3-3] || 0) + nums[3] = 2+1
+//         // so nums[3] is set to 3 and nums = [2,1,4,3,5,2,1]
+
+//         //when i = 4  nums[2]+nums[4] = 4+5 , nums([4-3] || 0) + nums[4] = 1+5
+//         // so nums[4] is set to 9 and nums = [2,1,4,3,9,2,1]
+
+//         //when i = 5  nums[3]+nums[5] = 3+2 , nums([5-3] || 0) + nums[5] = 4+2
+//         // so nums[5] is set to 6 and nums = [2,1,4,3,9,6,1]
+
+//         //when i = 6  nums[4]+nums[6] = 9+1 , nums([6-3] || 0) + nums[6] = 3+1
+//         // so nums[6] is set to 10 and nums = [2,1,4,3,9,6,10]
+
+//         nums[i] = Math.max(nums[i-2]+nums[i], (nums[i-3] || 0) + nums[i])
+//         console.log(nums)
+//     }
+//     return Math.max(nums[nums.length-1], nums[nums.length-2])
+
+// }
+
+// console.log(rob([2,1,2,1,5,2,1]))

1d195848016b4fe8e57a8da8d16d732b5b3ea2b0
timdean27 <timdean27@yahoo.com>
2022-10-26 16:46:03 -0400

    Completed has_path problem using DFSgit add .

--- Graphs/has_path.js
+++ Graphs/has_path.js
@@ -1,8 +1,27 @@
 // Write a function, hasPath, that takes in an object representing the adjacency list of a directed acyclic graph and two nodes (src, dst). 
 // The function should return a boolean indicating whether or not there exists a directed path between the source and destination nodes.
 
-// Hey. This is our first graph problem, so you should be liberal with watching the Approach and Walkthrough. Be productive, not stubborn. -AZ
 
+
+//we take in a source(starting point)
+//and a destination(eding point)
+//return if we can get to the ending node from the starting node
+
+const hasPath = (graph, src, dst) => {
+    const stack = [src]
+    let result = false
+    while (stack.length > 0){
+        console.log("stack",stack)
+        let current = stack.pop()
+        console.log("current", current)
+        for(let neighbor of graph[current]){
+            console.log("neighbor", neighbor)
+            stack.push(neighbor)
+            if(neighbor === dst){result = true}
+        }
+    }
+    return result
+}
 
 // test_00:
 const graph = {
@@ -13,8 +32,7 @@ const graph = {
     j: ['i'],
     k: []
   };
-  
-  hasPath(graph, 'f', 'k'); // true
+console.log(hasPath(graph, 'f', 'k')); // true
 
 //   test_01:
 const graph1 = {
@@ -26,7 +44,7 @@ const graph1 = {
     k: []
   };
   
-  hasPath(graph1, 'f', 'j'); // false
+console.log(hasPath(graph1, 'f', 'j')); // false
 
 
 //   test_02:
@@ -39,7 +57,7 @@ const graph1 = {
     k: []
   };
   
-  hasPath(graph2, 'i', 'h'); // true
+console.log(hasPath(graph2, 'i', 'h')); // true
 
   
 //   test_03:
@@ -51,7 +69,7 @@ const graph1 = {
     z: [],  
   };
   
-  hasPath(graph3, 'v', 'w'); // true
+console.log(hasPath(graph3, 'v', 'w')); // true
 
   
 //   test_04:
@@ -62,5 +80,6 @@ const graph1 = {
     y: ['z'],
     z: [],  
   };
-  
-  hasPath(graph4, 'v', 'z'); // false
+
+
+console.log(hasPath(graph4, 'v', 'z')); // false

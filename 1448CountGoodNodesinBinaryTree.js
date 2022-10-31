97a8bd516e0027ed70b2289752e6867076eee278
timdean27 <timdean27@yahoo.com>
2022-10-31 14:06:50 -0400

    1448. Count Good Nodes in Binary Tree

---
+++ 1448CountGoodNodesinBinaryTree.js
@@ -0,0 +1,62 @@
+// Given a binary tree root, a node X in the tree is named 
+//good if in the path from root to X there are no nodes with a value greater than X.
+
+// Return the number of good nodes in the binary tree.
+
+ 
+
+// Example 1:
+
+
+
+// Input: root = [3,1,4,3,null,1,5]
+// Output: 4
+// Explanation: Nodes in blue are good.
+// Root Node (3) is always a good node.
+// Node 4 -> (3,4) is the maximum value in the path starting from the root.
+// Node 5 -> (3,4,5) is the maximum value in the path
+// Node 3 -> (3,1,3) is the maximum value in the path.
+// Example 2:
+
+
+
+// Input: root = [3,3,null,4,2]
+// Output: 3
+// Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+// Example 3:
+
+// Input: root = [1]
+// Output: 1
+// Explanation: Root is considered as good.
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+   let count = 0
+   let max = root.val
+    console.log(max)
+   function dfs(root,max){
+        if(root === null) return;
+        if(root.val >= max){
+            max = Math.max(max , root.val);
+            count ++
+        }
+        dfs(root.left, max);
+        dfs(root.right, max);
+
+   }
+   dfs(root,max)
+
+   return count
+};
+
+console.log(goodNodes([3,1,4,3,null,1,5]));

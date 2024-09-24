7f98f2672fd304a44cc394992843a0b5b32b3890
timdean27 <timdean27@yahoo.com>
2024-09-24 10:03:37 -0400

    424 Longest Repeating character Replacement

--- Blind75/python/424_LONGEST_REPEATING_CHARACTER_REPLACMENT.py
+++ Blind75/python/424_LONGEST_REPEATING_CHARACTER_REPLACMENT.py
@@ -21,10 +21,30 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         hashMap = {}
-        left = right = 0 
-        
+        left = right = topFrequency = longest= 0 
 
+        while(right < len(s)):
 
+            # This attempts to retrieve the value for the key s[right] from the dictionary hashMap.
+            # If the key exists in hashMap, it returns the corresponding value.
+            # If the key does not exist, it returns the default value 0.
+            # Whether the value is retrieved (an existing count) or 0 (for a new character), it adds 1 to that value.
+            hashMap[s[right]] = hashMap.get(s[right], 0) + 1
+
+            # Update the topFrequency (most frequent character in the current window)
+            topFrequency = max(topFrequency , hashMap[s[right]])
+
+            # If the current window (right - left + 1) is invalid (i.e., too many characters to replace)
+            if (right - left + 1) - topFrequency > k:
+                # Shrink the window by moving the left pointer
+                hashMap[s[left]] -= 1
+                left += 1
+
+            # Update the longest valid substring found so far
+            longest = max(longest, right - left + 1)
+            # Move the right pointer to expand the window
+            right += 1
+        return longest
 
 
 sol = Solution()

b0972a03f950e756775bc5bc6cf4dfd9960e7646
timdean27 <timdean27@yahoo.com>
2024-11-25 08:26:54 -0500

    saving

--- Python/2070MostBeautiful_itemfor_each_query.py
+++ Python/2070MostBeautiful_itemfor_each_query.py
@@ -70,4 +70,4 @@ class Solution2:
         
         return output
 sol = Solution2
-print(sol.maximumBeauty2(items = [[1,2],[1,2],[1,3],[1,4]], queries = [1]))
+print(sol.maximumBeauty2(items = [[1,2],[3,2],[2,4],[5,6],[3,5]], queries = [1,2,3,4,5,6]))
